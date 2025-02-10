7855871b2ca9acb4721d1a60d14da955dd22a1de
Jefferson Guanabara <jefferson.guanabara@dcx.ufpb.br>
2025-02-10 11:35:53 -0300

    ball moviment

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 let canvas = document.getElementById('screen');
 let context = canvas.getContext('2d');
 
+/**
+ * variables relating construction background
+ */
+let backgroundWidth = 600;
+let backgroundHeigth = 400;
+let backgroundColor = 'black';
+
+/**
+ * variables relating to the ball and its movement  
+ */ 
+let initialPositionOfTheBallOnTheXAxis = backgroundWidth / 2;
+let initialPositionOfTheBallOnTheYAxis = backgroundHeigth / 2;
+let initialDiameterOfTheBall = 15;
+let ballColor = 'white';
 let speedBallX = 6;
 let speedBallY = 6;
 
@@ -12,14 +26,21 @@ function background(width, heigth, color) {
 function createBall(width, heigth, diameter) {
   context.beginPath();
   context.arc(width, heigth, diameter, 0, 2 * Math.PI);
-  context.fillStyle = 'white';
+  context.fillStyle = ballColor;
   context.fill();
   context.stroke();
 }
 
+function ballMoviment() {
+  while(true) {
+    createBall(initialPositionOfTheBallOnTheXAxis, initialPositionOfTheBallOnTheYAxis, initialDiameterOfTheBall);
+    initialPositionOfTheBallOnTheXAxis += speedBallX;
+  }
+}
+
 function setup() {
-  background(1200, 800, 'black');
-  createBall(50, 50, 15);
+  background(backgroundWidth, backgroundHeigth, backgroundColor);
+  ballMoviment();
 }
 
 setup();
